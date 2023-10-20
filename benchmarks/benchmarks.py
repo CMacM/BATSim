e96b9f5024831c91bedd2523d27815c123866c05
CMacM <macmahon14.02@gmail.com>
2023-10-20 10:52:25 -0400

    Fixed bug in benchmark code

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,13 +1,10 @@
-"""Two sample benchmarks to compute runtime and memory usage.
-
-For more information on writing benchmarks:
-https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
-
-import batsim
+"""Benchmarks to check computation time and memory usage for batsim."""
+import batsim.stamp as batstamp
+import batsim.transforms as batforms
 import galsim
 import time
 
-def compare_shear_speed():
+def time_shear_speed():
     
     # create galaxy
     gal = galsim.Sersic(n=1.5, half_light_radius=1.5, flux=40)
@@ -16,10 +13,11 @@ def compare_shear_speed():
     start = time.time()
     
     # generate stamp, shear and sample
-    gal_stamp = batsim.stamp.Stamp(nn=64, scale=0.2, centering='galsim')
-    lens = batsim.transforms.LensTransform(gamma1=0.2, center=[-0.5*scale, -0.5*scale])
+    gal_stamp = batstamp.Stamp(nn=640, scale=0.2, centering='galsim')
+    lens = batforms.LensTransform(gamma1=0.2, gamma2=0, kappa=0,
+                                  center=[-0.5*0.2, -0.5*0.2])
     gal_stamp.transform_grids(lens)
-    bat_array = gal_stamp.sample_galaxy(sersic_gal)
+    bat_array = gal_stamp.sample_galaxy(gal)
     
     end = time.time()
     
@@ -29,13 +27,13 @@ def compare_shear_speed():
     start = time.time()
     
     gal_shear = gal.shear(g1=0.2)
-    gal_array = gal_shear.drawImage(nx=64, ny=64, scale=0.2).array()
+    gal_array = gal_shear.drawImage(nx=640, ny=640, scale=0.2).array
     
     end = time.time()
     
     gal_time = end-start
     
-    return bat_time/gal_time
+    return {'batsim time': bat_time, 'galsim time' : gal_time}
 
 if __name__ == "__main__":
     test_affine_galsim()
